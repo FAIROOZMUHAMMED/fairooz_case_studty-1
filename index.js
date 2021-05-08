1a3d0cd8c104d3a14c5a3efa249122db1f9323d5
fairooz <fairoozmt@gmail.com>
2021-05-08 15:26:51 +0530

    First Commit : First Version

---
+++ index.js
@@ -0,0 +1,46 @@
+let userName= document.getElementById("userName");
+let Password=document.getElementById("Password");
+
+
+
+// Login
+function validateUsername(callback){
+    if(userName.value=="admin"){
+        return callback();
+    }else{
+      alert("Username/Password Mismatch!!!");
+        return false;
+    }
+  }
+  
+  function validatePassword(){
+    if(Password.value == "12345"){
+        return true;  
+    }
+   else{
+      alert("Username/Password Mismatch!!!");
+       return false;
+   } 
+  }
+  
+  // Todo
+
+function ajax(){
+ var xhttp=new XMLHttpRequest();
+ xhttp.onreadystatechange=function(){
+  if(this.readyState== 4&&this.status==200){
+      var data= JSON.parse(this.responseText);
+      console.log(this.responseText)
+      var tableData="";
+      for(var i =0; i<data.length; i++){
+        var currentData = data[i];
+        tableData += "<tr><td><input type='checkbox' name='test' value="+currentData.completed+"></td><td>"+currentData.title+"</td></tr>"
+    }
+      document.getElementById("myTable").innerHTML=tableData;
+      }
+     };  
+xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
+xhttp.send();
+}
+
+
