ed04b7b61054473878c73886b380a35b80e7681f
FAIROOZMUHAMMED <82165830+FAIROOZMUHAMMED@users.noreply.github.com>
2021-05-08 19:19:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,14 +36,14 @@ function ajax(){
         for(var i =0; i<data.length; i++){
         var currentData = data[i];
          tableData += `<tr><td class="tbln"><input type="checkbox"  ${chkchk(currentData.completed)} value=${currentData.title}>${currentData.title}</td></tr>`
-    }
+    };
       document.getElementById("myTable").innerHTML=tableData;
-     }
+     };
      };  
      function chkchk(val){
 console.log(val)  ;
 if(val==true){return "disabled"} else {return null ;}
-}  
+};
     var count =0;
      $('body').on('change','input[type=checkbox]',function(e){
          console.log("check box is checked");
